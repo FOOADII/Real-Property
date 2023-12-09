8b72416b3d44f76cdca19a714f8048bb4b677b51
FUAD GETACHEW <fuadgetachew2@gmail.com>
2023-12-09 09:43:59 +0300

    Creating user model

--- api/model/usermodel.js
+++ api/model/usermodel.js
@@ -4,5 +4,17 @@ const userSchema = new mongoose.Schema({
         type :String,
         required:true,
         unique:true,
+    },
+    email: {
+        type :String,
+        required:true,
+        unique:true,
+    },
+    password: {
+        type :String,
+        required:true,
+        
     }
-})
+}, {timestamps:true})
+const User = mongoose.model('User',userSchema)
+export default User;
