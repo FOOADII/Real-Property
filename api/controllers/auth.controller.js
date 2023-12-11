b6952f078c3a6a9b1c492ddade95e69c79b971f6
FUAD GETACHEW <fuadgetachew2@gmail.com>
2023-12-11 18:01:16 +0300

    sign in api router

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,43 @@
-import User from '../model/usermodel.js '
-import bcryptjs  from 'bcryptjs'
-//import { errorhandler } from '../utils/error'
+import User from '../model/usermodel.js'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-export const signup = async (req,res,next) => {
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+  const newUser = new User({ username, email, password: hashedPassword })
 
-    const { username ,email,password} = req.body
-    const hashedPassword = bcryptjs.hashSync(password,10 )
-    const newUser = new User ({username, email, password:hashedPassword})
-    try {
-          await newUser.save()
-   res.status(201).json('user is created successfully')
-
-    } catch (error) {
-       next(error)
-    }
- 
+  try {
+    await newUser.save()
+    res.status(201).json({ message: 'User created successfully' })
+  } catch (error) {
+    next(error)
+  }
 }
+
+export const SignIn = async (req, res, next) => {
+  const { email, password } = req.body
+
+  try {
+    const validUser = await User.findOne({ email })
+
+    if (!validUser) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
+    if (!validPassword) {
+      return res.status(401).json({ error: 'Wrong credentials' })
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const {password:pass,... rest } = validUser._doc 
+    rest
+
+    // Set the token as a cookie
+    res.cookie('access_token',token, { httpOnly: true })
+    res.status(200).json(rest)
+  } catch (error) {
+    next(error)
+  }
+};
